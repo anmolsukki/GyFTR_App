5fba6e7f67563d3a330a1d5a24758f8965caba53
anmol <anmol.s@gyftr.com>
2021-04-18 19:11:43 +0530

    Complete Code Push

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import SideDrawer from './src/Config/SideDrawer';
+import SplashScreen from 'react-native-splash-screen';
 import 'react-native-gesture-handler';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <SideDrawer />
